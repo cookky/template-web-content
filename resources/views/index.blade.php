e8cc412632d136933ef122f6f033dc70af07a982
cookky <cookky.developer@gmail.com>
2020-09-22 14:37:46 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,221 @@
+@extends('template')
+@section('title', 'Page Title')
+
+
+@section('content')
+<div class="container">
+    <div class="txt-header pt-5 pb-5">
+        <h1>Suggested Articles</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-sm-8">
+
+            <div class="card mb-3">
+                <div class="row no-gutters">
+                    <div class="col-12 col-sm-5">
+                        <div class="img">
+                            <img class="card-img" src="https://via.placeholder.com/500x700" alt="">
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-7">
+                        <div class="body">
+                            <div class="card-body">
+                                <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                                <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-12 col-sm-4">
+
+            <div class="col-12 clear-mobile">
+                <div class="sub-card">
+                    <div class="card mb-3">
+                        <div class="row no-gutters">
+                            <div class="body">
+                                <div class="card-body">
+                                    <h3 class="card-title">Make A Drink And We'll Tell You Where You'll Meet Your Soulmate</h3>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 clear-mobile">
+                <div class="sub-card">
+                    <div class="card mb-3">
+                        <div class="row no-gutters">
+                            <div class="body">
+                                <div class="card-body">
+                                    <h3 class="card-title">Make A Drink And We'll Tell You Where You'll Meet Your Soulmate</h3>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 clear-mobile">
+                <div class="sub-card">
+                    <div class="card mb-3">
+                        <div class="row no-gutters">
+                            <div class="body">
+                                <div class="card-body">
+                                    <h3 class="card-title">Make A Drink And We'll Tell You Where You'll Meet Your Soulmate</h3>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 clear-mobile">
+                <div class="sub-card">
+                    <div class="card mb-3">
+                        <div class="row no-gutters">
+                            <div class="body">
+                                <div class="card-body">
+                                    <h3 class="card-title">Make A Drink And We'll Tell You Where You'll Meet Your Soulmate</h3>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-sm-4">
+
+            <div class="card-content">
+                <div class="card">
+                    <img class="card-img-top" src="https://via.placeholder.com/500x700" alt="">
+                    <div class="card-body">
+                        <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                        <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-12 col-sm-4">
+
+            <div class="card-content">
+                <div class="card">
+                    <img class="card-img-top" src="https://via.placeholder.com/500x700" alt="">
+                    <div class="card-body">
+                        <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                        <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-12 col-sm-4">
+
+            <div class="card-content">
+                <div class="card">
+                    <img class="card-img-top" src="https://via.placeholder.com/500x700" alt="">
+                    <div class="card-body">
+                        <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                        <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="txt-header-blog pt-5 pb-5">
+            <h3>บทความทั่วไป</h3>
+        </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-12 col-sm-4">
+
+            <div class="card-content">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                        <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-4">
+
+            <div class="card-content">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                        <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-4">
+
+            <div class="card-content">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
+                        <h2 class="card-title">The Pleasure of Defying Food Fashions</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="{{ asset('css/template.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+
+
+<body>
+    <!-- decktop -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white d-none d-sm-block">
+        <div class="container pt-3 pb-3">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
+
+                <div class="justify-content-center w-100 d-none d-sm-block">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+
+                <div class="">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+
+    <!-- mobile -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white d-block d-sm-none ">
+        <div class="container pt-3 pb-3">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler navbar-toggler-first" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="toggler-mobile-close d-none">
+                <button class="navbar-toggler btn-lg btn-close" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 352 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
+                    </svg>
+                </button>
+            </div>
+
+            <div class="drawer-right-bg d-none">
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <div class="drawer-right">
+
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </nav>
+
+
+
+    @yield('content')
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#navbarSupportedContent').on('shown.bs.collapse', function() {
+                $('.toggler-mobile-close').removeClass("d-none");
+                $('.toggler-mobile-close').addClass("d-block");
+
+                // bg
+                $('.drawer-right-bg').removeClass("d-none");
+                $('.drawer-right-bg').addClass("d-block");
+            })
+
+            $('#navbarSupportedContent').on('hidden.bs.collapse', function() {
+                $('.toggler-mobile-close').removeClass("d-block");
+                $('.toggler-mobile-close').addClass("d-none");
+
+                // bg
+                $('.drawer-right-bg').removeClass("d-block");
+                $('.drawer-right-bg').addClass("d-none");
+            })
+        });
+    </script>
+</body>
+
+</html>
