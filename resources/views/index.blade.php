60d983d0648e31bb4b50ee8ca91cc6a80d896ce1
cookky <cookky.developer@gmail.com>
2020-09-25 00:42:00 +0700

    ss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,7 @@
                     <div class="col-12 col-sm-7">
                         <div class="body">
                             <div class="card-body">
-                                <h6 class="tag"><a href="/tags">#บทความทั่วไป</a></h6>
+                                <h6 class="tag"><a href="#">#บทความทั่วไป</a></h6>
                                 <h2 class="card-title"><a href="/show-blog">The Pleasure of Defying Food Fashions</a></h2>
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
